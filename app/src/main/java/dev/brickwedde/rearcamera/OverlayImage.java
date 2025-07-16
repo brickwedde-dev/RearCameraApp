ae2d512886d5ed4c388d549d14ef0f18d3fde019
Alexander Brickwedde <alex_git@brickwedde.de>
2025-07-16 22:25:59 +0200

    x

---
+++ app/src/main/java/dev/brickwedde/rearcamera/OverlayImage.java
@@ -0,0 +1,48 @@
+package dev.brickwedde.rearcamera;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class OverlayImage extends Service {
+
+    private WindowManager windowManager;
+    private ImageView chatHead;
+
+    @Override public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        chatHead = new ImageView(this);
+        chatHead.setImageResource(com.google.android.material.R.drawable.abc_ic_star_half_black_48dp);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        params.x = 0;
+        params.y = 100;
+
+        windowManager.addView(chatHead, params);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (chatHead != null) windowManager.removeView(chatHead);
+    }
+}
